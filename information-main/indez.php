6d5fbc543b583f9c2e9def7a4347ffaa8b368144
Arnie-Tricia <133929054+Arnie-Tricia@users.noreply.github.com>
2024-11-27 14:46:42 +0800

    Add files via upload

---
+++ information-main/indez.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+<?php
+    $firstName1 = "Arnie Tricia";
+    $lastName1 = "Sta. Ana";
+    $yourAge1 = "20";
+    $yourAddress1 = "Marikina";
+    $contactNo1 = "09123456789";
+
+    $firstName2 = "Xyza";
+    $lastName2 = "De Leon";
+    $yourAge2 = "19";
+    $yourAddress2 = "Morong, Rizal";
+    $contactNo2 = "09123456789";
+
+
+    $firstName3 = "Angelica";
+    $lastName3 = "Serrano";
+    $yourAge3 = "21";
+    $yourAddress3 = "Cainta, Rizal";
+    $contactNo3 = "09123456789";
+
+
+    $firstName4 = "Princess";
+    $lastName4 = "Apilado";
+    $yourAge4 = "21";
+    $yourAddress4 = "Antipolo, Rizal";
+    $contactNo4 = "09123456789";
+
+    ?>
+    <p><b>First Name:</b> <?php  echo "{$firstName1}" ;?></p>
+    <p><b>Last Name:</b> <?php  echo "{$lastName1}" ;?></p>
+    <p><b>Age:</b> <?php  echo "{$yourAge1}" ;?></p>
+    <p><b>Address:</b> <?php  echo "{$yourAddress1}" ;?></p>
+    <p><b>Contact:</b> <?php  echo "{$contactNo1}<br>" ;?></p>
+    <br>
+    <p><b>First Name:</b> <?php  echo "{$firstName2}" ;?></p>
+    <p><b>Last Name:</b> <?php  echo "{$lastName2}" ;?></p>
+    <p><b>Age:</b> <?php  echo "{$yourAge2}" ;?></p>
+    <p><b>Address:</b> <?php  echo "{$yourAddress2}" ;?></p>
+    <p><b>Contact:</b> <?php  echo "{$contactNo2}<br>" ;?></p>
+    <br>
+    <p><b>First Name:</b> <?php  echo "{$firstName3}" ;?></p>
+    <p><b>Last Name:</b> <?php  echo "{$lastName3}" ;?></p>
+    <p><b>Age:</b> <?php  echo "{$yourAge3}" ;?></p>
+    <p><b>Address:</b> <?php  echo "{$yourAddress3}" ;?></p>
+    <p><b>Contact:</b> <?php  echo "{$contactNo3}<br>" ;?></p>
+    <br>
+    <p><b>First Name:</b> <?php  echo "{$firstName4}" ;?></p>
+    <p><b>Last Name:</b> <?php  echo "{$lastName4}" ;?></p>
+    <p><b>Age:</b> <?php  echo "{$yourAge4}" ;?></p>
+    <p><b>Address:</b> <?php  echo "{$yourAddress4}" ;?></p>
+    <p><b>Contact:</b> <?php  echo "{$contactNo4}<br>" ;?></p>
+    <br>
+    
+</body>
+</html>
